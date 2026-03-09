d1d85ef18e9dc8a590bbbee198c0450759351f33
H.C. Cheung <hccheung117@gmail.com>
2026-03-09 17:08:12 +0000

    feat(release): add automated changelog generation and release script
    Implements a release script for semver bumping and a GitHub workflow enhancement that generates structured changelogs organized by commit type (feat, fix, refactor, etc.) from the git log since the last tag.

---
+++ release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+echo ""
+node -e "
+const s = require('semver'), v = require('./package.json').version;
+console.log('  Current version: ' + v);
+console.log();
+['patch','minor','major','prepatch','preminor','premajor','prerelease'].forEach((t, i) =>
+  console.log('  ' + (i + 1) + ') ' + t.padEnd(12) + ' → ' + s.inc(v, t)));
+"
+echo ""
+
+read -p "  Pick one: " CHOICE
+TYPES=(patch minor major prepatch preminor premajor prerelease)
+TYPE="${TYPES[$((CHOICE - 1))]}"
+
+if [ -z "$TYPE" ]; then
+  echo "  Invalid choice."
+  exit 1
+fi
+
+echo ""
+npm version "$TYPE"
+git push && git push --tags
+echo ""
+echo "  Done! Release triggered."
+echo ""
